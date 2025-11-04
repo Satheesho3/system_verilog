cc52b4c46f0e23668a03242564a8521b12457103
Satheesh M <sk2355839@gmail.com>
2025-11-04 12:06:51 +0530

    Create priority_example.sv

---
+++ day_09/priority_example.sv
@@ -0,0 +1,16 @@
+module priority_example;
+  int a,b;
+  initial begin
+    a=20;
+    b=30;
+    priority if(a<b)
+      $display("1");
+    else if(a<10)
+      $display("2");
+    else if(a<30)
+      $display("4");
+    else
+      $display("3");
+   
+  end
+endmodule
