2b0c7ab243becf17871d79accdf135e57cdae645
Satheesh M <sk2355839@gmail.com>
2025-11-05 16:30:34 +0530

    Create fork_join_any.sv

---
+++ day_10/fork_join_any.sv
@@ -0,0 +1,21 @@
+module fork_join_any;
+  initial begin
+    $display("t=%0t Start of simulation", $time);
+
+    fork
+      begin
+        #5 $display("Process 1 finished at time %0t", $time);
+      end
+
+      begin
+        #10 $display("Process 2 finished at time %0t", $time);
+      end
+
+      begin
+        #2 $display("Process 3 finished at time %0t", $time);
+      end
+    join_any
+
+    $display("Time %0t: All processes completed", $time);
+  end
+endmodule
