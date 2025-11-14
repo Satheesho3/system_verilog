b32a8e22dcff09b79da43d164ff411651af1cefc
Satheesh M <sk2355839@gmail.com>
2025-11-14 18:13:10 +0530

    Add event wait example in event_wait.sv

---
+++ day_14/event_wait.sv
@@ -0,0 +1,21 @@
+module parent;
+  event e1;
+  task parent_A();
+    
+    $display("t=%t 1",$time);
+    ->e1;
+    $display("t=%t 2",$time);
+    
+  endtask
+  task parent_B();
+    $display("t=%t 3",$time);
+    wait(e1.triggered);
+    $display("t=%t triggered",$time);
+  endtask
+  initial begin
+    fork
+    parent_A();
+    parent_B();
+    join
+  end
+endmodule
