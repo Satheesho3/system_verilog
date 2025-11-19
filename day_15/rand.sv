6ddcd8153b6358bc8b02170a527fe62dd22e2b93
Satheesh M <sk2355839@gmail.com>
2025-11-19 15:17:09 +0530

    Create rand.sv

---
+++ day_15/rand.sv
@@ -0,0 +1,15 @@
+class parent;
+  rand bit [1:0]a;
+ 
+endclass
+module top;
+  parent r;
+  initial begin
+    r=new();
+    repeat(10)begin
+    r.randomize();
+      $display(" a=%0d ",r.a);
+    end
+  end
+endmodule
+    
