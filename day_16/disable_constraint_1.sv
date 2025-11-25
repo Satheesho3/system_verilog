ff3915c985a00af98cb95a60f317c0004b02a03b
Satheesh M <sk2355839@gmail.com>
2025-11-25 18:39:21 +0530

    Create disable_constraint_1.sv

---
+++ day_16/disable_constraint_1.sv
@@ -0,0 +1,19 @@
+class parent;
+  rand bit[7:0]a;
+  rand bit[7:0]b;
+  constraint range{a inside {[1:30]};}
+  constraint range1{b inside {20,30,40};}
+endclass
+module top;
+  parent p;
+  initial begin
+    p=new();
+    p.randomize();
+  $display("a=%0d b=%0d",p.a,p.b);
+   
+    p.range1.constraint_mode(0);
+     p.randomize();
+    $display("a=%0d b=%0d",p.a,p.b);
+    end
+  
+endmodule
